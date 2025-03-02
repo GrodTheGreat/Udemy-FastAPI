1c023ee87d0b199d4497947adc91725e69a13583
Gerard Causse <grod.causse@gmail.com>
2025-03-02 13:15:27 -0500

    Add encapsulation

--- Refresher/OOP/enemy.py
+++ Refresher/OOP/enemy.py
@@ -9,15 +9,18 @@ class Enemy:
 
     # Parameter Constructor. Pass in values for the class to use to instantiate.
     def __init__(self, type_of_enemy, health_points=10, attack_damage=1):
-        self.type_of_enemy = type_of_enemy
+        self.__type_of_enemy = type_of_enemy
         self.health_points = health_points
         self.attack_damage = attack_damage
 
+    def get_type_of_enemy(self):
+        return self.__type_of_enemy
+
     def talk(self):
-        print(f"I am a {self.type_of_enemy}. Be prepared to fight!")
+        print(f"I am a {self.__type_of_enemy}. Be prepared to fight!")
 
     def walk_forward(self):
-        print(f"{self.type_of_enemy} moves closer to you")
+        print(f"{self.__type_of_enemy} moves closer to you")
 
     def attack(self):
-        print(f"{self.type_of_enemy} attacks for {self.attack_damage} damage")
+        print(f"{self.__type_of_enemy} attacks for {self.attack_damage} damage")
