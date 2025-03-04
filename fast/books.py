d2671bf46248669facc266b51d99b0d353addcf3
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2025-03-04 15:26:53 -0500

    Compelte Data Validation Path Parameters lesson

--- fast/books.py
+++ fast/books.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 from pydantic import BaseModel, Field
 
 app = FastAPI()
@@ -68,7 +68,7 @@ async def read_all_books():
 
 
 @app.get("/books/{book_id}")
-async def read_book(book_id: int):
+async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
@@ -119,7 +119,7 @@ async def update_book(book: BooKRequest):
 
 
 @app.delete("/books/{book_id}")
-async def delete_book(book_id: int):
+async def delete_book(book_id: int = Path(gt=0)):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
