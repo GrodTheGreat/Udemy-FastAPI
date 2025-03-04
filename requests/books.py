5b331088dbe7f5227924cef0c4df716ee8f263e4
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2025-03-03 20:35:54 -0500

    Complete Path Parameters lesson

--- requests/books.py
+++ requests/books.py
@@ -15,3 +15,15 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get("title").casefold() == book_title.casefold():
+            return book
+
+
+@app.get("/books/mybook")
+async def read_all_books():
+    return {"book_title": "My favorite book!"}
