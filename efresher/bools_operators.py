17b727de1b9147e21b30c11147d6120afc53c1a0
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2025-03-03 20:20:52 -0500

    Add files via upload

---
+++ efresher/bools_operators.py
@@ -0,0 +1,26 @@
+"""
+Booleans and Operators
+"""
+
+like_coffee = True
+like_tea = False
+print(like_coffee)
+
+fav_food = "Pizza"
+fav_num = "32"
+print(type(like_coffee))
+print(type(fav_food))
+print(type(fav_num))
+
+# Comparison operators
+print(1 == 2)
+print(1 != 2)
+print(1 > 2)
+print(1 < 2)
+print(1 >= 1)
+print(1 <= 2)
+
+# Logical operators
+print(1 > 3 and 5 < 7)
+print(1 > 3 or 5 < 7)
+print(not (1 == 1))
