dd97a2c7ae62445415bb9c3b5f12b551f9547f09
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2025-03-04 21:12:14 -0500

    Add files via upload

--- todos/models.py
+++ todos/models.py
@@ -1,12 +1,27 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 
 from database import Base
 
 
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
+
 class Todo(Base):
     __tablename__ = "todo"
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("user.id"))
