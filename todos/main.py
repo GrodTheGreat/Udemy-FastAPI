8673fa395e30c91ef95cad270d01d2fd0f72d2a9
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2025-03-05 21:07:07 -0500

    Complete Authenticate Requests section

--- todos/main.py
+++ todos/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from database import engine
 import models
-from routers import admin, auth, todo
+from routers import admin, auth, todo, user
 
 app = FastAPI()
 
@@ -11,3 +11,4 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(admin.router)
 app.include_router(auth.router)
 app.include_router(todo.router)
+app.include_router(user.router)
