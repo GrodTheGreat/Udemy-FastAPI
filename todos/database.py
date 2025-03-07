3740288f4040266eb9c4577b889e4d8719f02442
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2025-03-06 21:16:25 -0500

    Complete Postgres lessons

--- todos/database.py
+++ todos/database.py
@@ -2,10 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db" # sqlite
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql://postgres:test1234!@localhost/TodoApplicationDatabase"
+)
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} # sqlite
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
