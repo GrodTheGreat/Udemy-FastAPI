65352d11b96fa8c02d2ee65594625a7f71ba104f
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2025-03-06 21:50:43 -0500

    Compelte MySQL lessons

--- todos/database.py
+++ todos/database.py
@@ -4,8 +4,13 @@ from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db" # sqlite
 SQLALCHEMY_DATABASE_URL = (
+    # postgres
     "postgresql://postgres:test1234!@localhost/TodoApplicationDatabase"
 )
+# SQLALCHEMY_DATABASE_URL = (
+#     # mysql
+#     "mysql+pymysql://root:test1234!@127.0.0.1:3306/TodoApplicationDatabase"
+# )
 
 engine = create_engine(
     # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} # sqlite
