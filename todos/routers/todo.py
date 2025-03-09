7afc2a3fa6dda244aeaa6556b2f72aed938b05df
Gerard Causse <grod.causse@gmail.com>
2025-03-09 15:35:52 -0400

    Complete Edit Todo lesson

--- todos/routers/todo.py
+++ todos/routers/todo.py
@@ -81,6 +81,23 @@ async def render_add_todo_page(request: Request):
         return redirect_to_login()
 
 
+@router.get("/edit-todo-page/{todo_id}")
+async def render_edit_todo_page(request: Request, todo_id: int, db: db_dependency):
+    try:
+        user = await get_current_user(request.cookies.get("access_token"))
+
+        if user is None:
+            return redirect_to_login()
+
+        todo = db.query(Todo).filter(Todo.id == todo_id).first()
+
+        return templates.TemplateResponse(
+            "edit-todo.html", {"request": request, "user": user, "todo": todo}
+        )
+    except:
+        return redirect_to_login()
+
+
 ### Endpoints ###
 @router.get("/", status_code=status.HTTP_200_OK)
 async def read_all(user: user_dependency, db: db_dependency):
