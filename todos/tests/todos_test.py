fb4d47fcb667793545f941c8ad3e2e336a26d1c3
Gerard Causse <grod.causse@gmail.com>
2025-03-08 20:37:33 -0500

    Complete FastAPI Test Complete lesson

---
+++ todos/tests/todos_test.py
@@ -0,0 +1,79 @@
+import os
+import sys
+
+from fastapi import status
+from fastapi.testclient import TestClient
+import pytest
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from database import Base
+from main import app
+from models import Todo
+from routers.todo import get_current_user, get_db
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def override_get_current_user():
+    return {"username": "codingwithrobytest", "id": 1, "role": "admin"}
+
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+client = TestClient(app=app)
+
+
+@pytest.fixture
+def test_todo():
+    todo = Todo(
+        title="Learn to code",
+        description="Need to learn everyday",
+        priority=5,
+        complete=False,
+        owner_id=1,
+    )
+
+    db = TestingSessionLocal()
+    db.add(todo)
+    db.commit()
+    yield todo
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM todo;"))
+        connection.commit()
+
+
+def test_read_all_authenticated(test_todo):
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == [
+        {
+            "id": 1,
+            "title": "Learn to code",
+            "description": "Need to learn everyday",
+            "priority": 5,
+            "complete": False,
+            "owner_id": 1,
+        }
+    ]
