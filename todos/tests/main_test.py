ddb45a55b30e9869d9148ba2de73fdd1897d19ea
Gerard Causse <grod.causse@gmail.com>
2025-03-09 15:57:44 -0400

    Reorder imports

--- todos/tests/main_test.py
+++ todos/tests/main_test.py
@@ -1,12 +1,10 @@
-from fastapi.testclient import TestClient
-from fastapi import status
-
 import sys
 import os
 
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-)
+from fastapi.testclient import TestClient
+from fastapi import status
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from main import app
 
